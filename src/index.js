0569643e04c7e1141114986649391af5ff755fc2
Srishti Singh <srish17816@gmail.com>
2025-08-11 16:24:36 +0530

    Custom API Response and Error Handling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ import connectDB from "./db/index.js";
 dotenv.config({path : './.env'})
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR ::", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : 
+            ${process.env.PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log("MONGODB Connection FAILED !!!", error);
+    
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+
+// promise wala code :-
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = (func) => {}
+// const asyncHandler = (func) => { return () => {}}
+// const asyncHandler = (func) => () => {} ... remove outermost bracket
+
+
+
+// async await wala code :-
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({ // we send this status in which we send error code also send json response
+//             success : false,
+//             message : error.message
+//         })
+//     }
+// }
+
+
+
+
