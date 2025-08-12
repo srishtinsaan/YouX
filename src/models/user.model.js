34914dcc5c3862a4cc5e34eab3a6df3e5c98cd9a
Srishti Singh <srish17816@gmail.com>
2025-08-12 17:54:19 +0530

    setup completed till multer

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -83,7 +83,7 @@ userSchema.methods.generateRefreshToken = async function(){
         //payload
         {
             _id : this._id,
-        },
+        }, 
         process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn : process.env.REFRESH_TOKEN_EXPIRY
